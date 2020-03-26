9d2d7c71eaf59349f64bef87f2db39d33cd71f65
qing-feng.zhao <fairy_xingyun@hotmail.com>
2020-03-26 11:54:07 +0800

    init project

---
+++ read-code-source-sample/src/test/java/com/xingyun/readcodesourcesample/ReadCodeSourceSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xingyun.readcodesourcesample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadCodeSourceSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
